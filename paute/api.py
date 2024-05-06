9af1c0785b350065d6be1eca6837a293471ceba4
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-06 16:25:56 -0500

    Added title and logo

--- paute/api.py
+++ paute/api.py
@@ -260,6 +260,7 @@ def get_forecast_stats(stats, rperiods, comid, records, sim):
     
     plot_data.update(rperiods.to_dict(orient='index').items())
     max_visible = max(max(plot_data['flow_max']), max(plot_data['flow_avg']), max(plot_data['high_res']))
+    max_visible = max_visible + 0.15*max_visible
     #rperiod_scatters = _rperiod_scatters(startdate, enddate, rperiods, plot_data['y_max'], max_visible)
 
     scatter_plots = [
@@ -328,7 +329,7 @@ def get_forecast_stats(stats, rperiods, comid, records, sim):
     titulo = "Rio Paute (en Paute) - Estación H0894"
     layout = go.Layout(
         title=titulo,
-        yaxis={'title': 'Nivel (m)', 'range': [0, 'auto']},
+        yaxis={'title': 'Nivel (m)', 'range': [0, max_visible]},
         xaxis={'title': 'Fecha (UTC +0:00)', 'range': [startdate, enddate], 'hoverformat': '%b %d %Y %H:%M',
                'tickformat': '%b %d %Y'},
     )
@@ -346,8 +347,8 @@ def get_forecast_stats(stats, rperiods, comid, records, sim):
         y=1,
         xref="paper",
         yref="paper",
-        sizex=0.1,
-        sizey=0.1,
+        sizex=1,
+        sizey=1,
         xanchor="left",
         yanchor="top"
     )
