b17f979513fb9589a571ce7c66e7dfd7141e4108
Juseth Chancay <juseth.chancay@gmail.com>
2024-07-04 10:51:01 -0500

    Added routines for sgr report

---
+++ sgr/mail.py
@@ -0,0 +1,39 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def send(subject, body, attachment_file, sender, password):
+    # Users to send email
+    recipients = ["jusethchancay@ecociencia.org"]
+    #
+    # SMTP server
+    smtp_server = "smtp.office365.com"
+    smtp_port = 587
+    #
+    # Configure the message
+    message = MIMEMultipart()
+    message['From'] = sender
+    message['To'] = ", ".join(recipients)
+    message['Subject'] = subject
+    #
+    # Attach the email body
+    message.attach(MIMEText(body, 'plain'))
+    #
+    # Attach the PDF file
+    attachment = open(attachment_file, 'rb')
+    attachment_part = MIMEBase('application', 'octet-stream')
+    attachment_part.set_payload((attachment).read())
+    encoders.encode_base64(attachment_part)
+    attachment_part.add_header('Content-Disposition', "attachment; filename= %s" % attachment_file)
+    message.attach(attachment_part)
+    #
+    # Connect to the SMTP server and send the email
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(sender, password)
+    server.sendmail(sender, recipients, message.as_string())
+    server.quit()
+
+
---
+++ sgr/main.py
@@ -0,0 +1,37 @@
+import os
+import plot
+import warnings
+import pandas as pd
+import datetime as dt
+import geopandas as gpd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+
+
+
+
+
+# Ignore warnings
+warnings.filterwarnings("ignore")
+
+# Change the work directory
+user = os.getlogin()
+user_dir = os.path.expanduser('~{}'.format(user))
+os.chdir(user_dir)
+os.chdir("tethys_apps_ecuador/geoglows_database_ecuador")
+
+# Read SHP files
+ec = gpd.read_file("shp/ecuador_diss.shp")
+prov = gpd.read_file("shp/ecuador.shp")
+area = gpd.read_file("shp/area_afectada.shp")
+
+
+
+
+
+os.chdir(user_dir)
+os.chdir("tethys_apps_ecuador/geoglows_database_ecuador")
+
+raster = "https://www.hydroshare.org/resource/925ad37f78674d578eab2494e13db240/data/contents/pacum_persiann_daily7.tif"
+plot.pacum_ec(raster=raster, ec_gdf=ec, prov_gdf=prov, paute_gdf=area)
